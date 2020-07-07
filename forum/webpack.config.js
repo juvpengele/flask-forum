d9cab0d98a0d559824cfa393ce9674a11f4efab5
Juvenal Pengele <pengeleglodiejuvenal@gmail.com>
2020-07-07 23:44:28 +0000

    Start the integration of babel

--- forum/webpack.config.js
+++ forum/webpack.config.js
@@ -4,5 +4,20 @@ module.exports = {
     output: {
         path: __dirname + '/public/js',
         filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-object-rest-spread']
+                    }
+                }
+            }
+        ]
     }
 };
