75dd609029888917bcb449e3303e89d24cae74cd
Juvenal Pengele <pengeleglodiejuvenal@gmail.com>
2020-07-08 06:12:50 +0000

    Add izitoast dependencies

--- forum/assets/js/bootstrap.js
+++ forum/assets/js/bootstrap.js
@@ -2,3 +2,5 @@ window.$ = require('jquery');
 
 require('popper.js');
 require('bootstrap');
+
+window.iziToast = require('izitoast');
--- forum/webpack.config.js
+++ forum/webpack.config.js
@@ -14,8 +14,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-object-rest-spread']
-                    }
+                    },
                 }
             }
         ]
