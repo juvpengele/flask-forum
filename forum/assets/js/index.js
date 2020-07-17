600224060e8d783672e2ac2ff4fa88f68a5a2f76
Juvenal Pengele <pengeleglodiejuvenal@gmail.com>
2020-07-17 18:16:48 +0000

    Integrate vuejs in webpack

--- forum/assets/js/index.js
+++ forum/assets/js/index.js
@@ -1,2 +1,10 @@
+import Vue from "vue";
+
 require('./bootstrap');
 require('./utils');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: "#root",
+});
--- forum/webpack.config.js
+++ forum/webpack.config.js
@@ -1,12 +1,21 @@
-module.exports = {
+const dev = process.env.NODE_ENV === "dev";
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const config = {
     mode: 'development',
     entry: "./assets/js/index.js",
     output: {
         path: __dirname + '/public/js',
         filename: '[name].js'
     },
+    watch: dev,
+    devtool: dev ? "cheap-module-eval-source-map": "source-map",
     module: {
         rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
             {
                 test: /\.m?js$/,
                 exclude: /(node_modules)/,
@@ -16,7 +25,17 @@ module.exports = {
                         presets: ['@babel/preset-env'],
                     },
                 }
-            }
+            },
         ]
-    }
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+        }
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 };
+
+module.exports = config;
