6ffb7a9456715f9fab513a461a804435efc46f7d
Juvenal Pengele <pengeleglodiejuvenal@gmail.com>
2020-10-20 08:55:51 +0000

    Features implemented
    - Ignore avatar files
    - Ignore cache file generated
    - Set cache property as _cache

--- forum/src/cache.py
+++ forum/src/cache.py
@@ -6,12 +6,12 @@ from werkzeug.contrib.cache import FileSystemCache
 class Cache:
     def __init__(self):
         cache_directory = os.path.join(os.getcwd(), 'forum', 'src', 'storage', 'cache')
-        self.cache = FileSystemCache(cache_directory, default_timeout=10*60)
+        self._cache = FileSystemCache(cache_directory, default_timeout=10*60)
 
     def set(self, key, value, timeout=None):
        
         try:
-            return self.cache.set(key, json.dumps(value), timeout=timeout)
+            return self._cache.set(key, json.dumps(value), timeout=timeout)
 
         except Exception as exception:
             print("An error occured")
@@ -19,7 +19,7 @@ class Cache:
 
     def get(self, key):
         try:
-            data = self.cache.get(key)
+            data = self._cache.get(key)
 
             if data:
                 return json.loads(data)
@@ -29,4 +29,4 @@ class Cache:
 
 
     def has(self, key):
-        return self.cache.has(key) is not None
+        return self._cache.has(key) is not None
