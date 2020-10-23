688f77cca0db1e4375b11c715011ace28cd7842e
Juvenal Pengele <pengeleglodiejuvenal@gmail.com>
2020-10-23 05:58:22 +0000

    Refactor the cache class by extract a static method to get cache directory

--- forum/src/cache.py
+++ forum/src/cache.py
@@ -5,22 +5,23 @@ from werkzeug.contrib.cache import FileSystemCache
 
 class Cache:
     def __init__(self):
-        cache_directory = os.path.join(os.getcwd(), 'forum', 'src', 'storage', 'cache')
-        self.cache = FileSystemCache(cache_directory, default_timeout=10*60)
+        self.cache = FileSystemCache(Cache.cache_dir(), default_timeout=10*60)
+
+    @staticmethod
+    def cache_dir():
+        return os.path.join(os.getcwd(), 'forum', 'src', 'storage', 'cache')
 
     def set(self, key, value, timeout=None):
-       
         try:
             return self.cache.set(key, json.dumps(value), timeout=timeout)
 
         except Exception as exception:
-            print("An error occured")
+            print(exception)
 
 
     def get(self, key):
         try:
             data = self.cache.get(key)
-
             if data:
                 return json.loads(data)
 
